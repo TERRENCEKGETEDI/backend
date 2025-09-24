f8347e90917dd69462a06abd7ac92c72ef30cb09
BuhlePMkhoba <52975444@mynwu.ac.za>
2025-09-24 03:55:32 +0200

    Update RegisterDto.cs

--- DTOs/RegisterDto.cs
+++ DTOs/RegisterDto.cs
@@ -5,5 +5,5 @@ namespace UserManagementApp.DTOs
         public string Email { get; set; }
         public string FullName { get; set; }
         public string Password { get; set; }
-        public string UniversityId { get; set; }
+        public string UnivarsityId { get; set; }
     }
