a2620c5e3e478c063788b190e83d1d3ced3f359a
BuhlePMkhoba <52975444@mynwu.ac.za>
2025-09-24 02:59:13 +0200

    Create RegisterDto.cs

---
+++ DTOs/RegisterDto.cs
@@ -0,0 +1,7 @@
+public class RegisterDto
+{
+    public string Email { get; set; }
+    public string FullName { get; set; }
+    public string Password { get; set; }
+    public string UniversityId { get; set; }
+}
