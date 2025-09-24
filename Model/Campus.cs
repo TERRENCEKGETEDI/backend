51f98301625713debcd497f2062f16df796fa07c
BuhlePMkhoba <52975444@mynwu.ac.za>
2025-09-24 03:35:28 +0200

    Update Campus.cs

--- Model/Campus.cs
+++ Model/Campus.cs
@@ -1,8 +1,8 @@
 public class Campus
 {
-    [Key] public Guid Id { get; set; }
-    [Required] public Guid UniversityId { get; set; }
+    [Key] public Guid CampusId { get; set; }
+    [Required] public Guid UnivarsityId { get; set; }
     [MaxLength(10)] public string CampusCode { get; set; }
     [MaxLength(255)] public string Name { get; set; }
-    [ForeignKey("UniversityId")] public University University { get; set; }
+    [ForeignKey("UnivarsityId")] public Uniaersity Univarsity { get; set; }
 }
