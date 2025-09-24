c00a3c3b89f6eeb4d64e326a330102f33269ece4
BuhlePMkhoba <52975444@mynwu.ac.za>
2025-09-24 03:03:51 +0200

    Create PasswordValidator.cs

---
+++ Helpers/PasswordValidator.cs
@@ -0,0 +1,11 @@
+public static class PasswordValidator
+{
+    public static bool IsValid(string password)
+    {
+        if (string.IsNullOrWhiteSpace(password)) return false;
+        if (password.Length < 8 || password.Length > 15) return false;
+        if (!Regex.IsMatch(password, @"[A-Z]")) return false;
+        if (!Regex.IsMatch(password, @"[!@#$%^&*(),.?""{}|<>_\-+=]")) return false;
+        return true;
+    }
+}
