a0ffcd8ac1558e7ac6e66c1bd3a8ca457819810f
BuhlePMkhoba <52975444@mynwu.ac.za>
2025-09-24 03:57:55 +0200

    Update Univarsity.cs

--- Model/Univarsity.cs
+++ Model/Univarsity.cs
@@ -1,6 +1,6 @@
 public class Univarsity
 {
-    [Key] [MaxLength(10)] public Guid Id { get; set; }
+    [Key] [MaxLength(10)] public Guid Id { get; set; } = Guid.NewGuid();
     [Required, MaxLength(255)] public string Name { get; set; }
     [MaxLength(255)] public string? CampusName { get; set; }
     [MaxLength(100)] public string? Province { get; set; }
